2fa1451b0e05df96a15210abe966e65cf09a392b
Jakob Grøhn <bokajgd@gmail.com>
2023-03-02 01:41:07 +0100

    feat: adding admissions loaders

---
+++ src/features/flattened_features/loaders/load_admissions.py
@@ -0,0 +1,107 @@
+"""Loaders for the admissions table."""
+from typing import Optional
+
+import pandas as pd
+
+from utils import data_loaders, load_dataset_from_file, DATA_PATH
+
+
+@data_loaders.register("admissions")
+def load_admissions(
+    nrows: Optional[int] = None,
+    return_value_as_admission_length_days: bool = False,
+) -> pd.DataFrame:
+    """Load admissions table. Drops admissions without chartevents data and duplicate rows. 
+    Returns a df with columns for patient_id, timestamp, admission_type, and value.
+
+    Args:
+        nrows (int): Number of rows to load.
+        return_value_as_admission_length_days (bool): If True, returns the length of the
+            admission in days instead. If not it returns 1 for all admissions. Defaults to False.
+
+    Returns:
+        pd.DataFrame: Admissions table.
+    """
+    admissions_file_path = DATA_PATH / "raw" / "ADMISSIONS.csv"
+
+    admissions = load_dataset_from_file(
+        file_path=admissions_file_path,
+        nrows=nrows,
+        cols_to_load=[
+            "SUBJECT_ID",
+            "ADMITTIME",
+            "DISCHTIME",
+            "ADMISSION_TYPE",
+            "HAS_CHARTEVENTS_DATA",
+        ],
+    )
+
+    # Dropping admissions without chartevents data
+    admissions = admissions[admissions["HAS_CHARTEVENTS_DATA"] == 1].drop(
+        columns=["HAS_CHARTEVENTS_DATA"]
+    )
+
+    # Drop duplicates
+    admissions.reset_index(drop=True).drop_duplicates(
+        subset=["SUBJECT_ID", "ADMITTIME", "DISCHTIME", "ADMISSION_TYPE"],
+        keep="first",
+    )
+
+    # Add value column for feature generation
+    if return_value_as_admission_length_days:
+        admissions["value"] = (
+            admissions["DISCHTIME"] - pd.to_datetime(admissions["ADMITTIME"])
+        ).dt.total_seconds() / 86400
+    else:
+        admissions["value"] = 1
+    
+    # Drop admission time
+    admissions = admissions.drop(columns="ADMITTIME")
+
+    # Renaming columns
+    admissions = admissions.rename(
+        columns={
+            "SUBJECT_ID": "patient_id",
+            "DISCHTIME": "timestamp",
+            "ADMISSION_TYPE": "admission_type",
+        }
+    )
+
+    return admissions.reset_index(drop=True)
+
+
+
+
+@data_loaders.register("emergency_admissions")
+def load_emergency_admissions(
+    nrows: Optional[int] = None,
+    return_value_as_admission_length_days: bool = True,
+    timestamps_only: bool = False,
+) -> pd.DataFrame:
+    """Load emergency admissions table.
+
+    Args:
+        nrows (int): Number of rows to load.
+        return_value_as_admission_length_days (bool): If True, returns the length of the
+            admission in days instead. If not it returns 1 for all admissions. Defaults to True.
+        timestamps_only (bool): If True, returns only the timestamps and patient_id. 
+            If not it also returns the value column Defaults to False.
+
+    Returns:
+        pd.DataFrame: Emergency admissions table.
+    """
+    admissions = load_admissions(nrows=nrows, return_value_as_admission_length_days = return_value_as_admission_length_days)
+
+    emergency_admissions = admissions[admissions["admission_type"] == "EMERGENCY"].drop(
+        columns=["admission_type"]
+    )
+
+    if timestamps_only:
+        return emergency_admissions.drop(columns="value").reset_index(drop=True)
+    else:
+        return emergency_admissions.reset_index(drop=True)
+    
+
+
+if __name__ == "__main__":
+    admissions = load_admissions(nrows=100)
--- src/features/flattened_features/loaders/utils.py
+++ src/features/flattened_features/loaders/utils.py
@@ -1,4 +1,4 @@
-"""A set of misc. utility functions for generating flattened features. """
+"""A set of misc. utility functions for data loaders. """
 
 from pathlib import Path
 from typing import Any, Optional
@@ -8,19 +8,21 @@ import pandas as pd
 
 data_loaders = catalogue.create("timeseriesflattener", "data_loaders")
 
-RELATIVE_PROJECT_ROOT = Path(__file__).resolve().parents[3]
+RELATIVE_PROJECT_ROOT = Path(__file__).resolve().parents[4]
 DATA_PATH = RELATIVE_PROJECT_ROOT / "data"
 
 
 def load_dataset_from_file(
     file_path: Path,
     nrows: Optional[int] = None,
+    cols_to_load: Optional[list[str]] = None,
 ) -> pd.DataFrame:
     """Load dataset from file. Handles csv and parquet files based on suffix.
 
     Args:
         file_path (str): File name.
         nrows (int): Number of rows to load.
+        cols_to_load (list[str]): Columns to load.
 
     Returns:
         pd.DataFrame: Dataset
@@ -29,7 +31,10 @@ def load_dataset_from_file(
     file_suffix = file_path.suffix
 
     if file_suffix == ".csv":
-        return pd.read_csv(file_path, nrows=nrows)
+        if cols_to_load:
+            return pd.read_csv(file_path, nrows=nrows, usecols=cols_to_load)
+        else:
+            return pd.read_csv(file_path, nrows=nrows)
 
     elif file_suffix == ".parquet":
         if nrows:
