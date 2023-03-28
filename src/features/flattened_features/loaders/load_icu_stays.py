074f7b1392c2cdfa921aaa43b3ec77cf951e0707
Jakob Grøhn <bokajgd@gmail.com>
2023-03-28 12:02:32 +0200

    style: lint

--- src/features/flattened_features/loaders/load_icu_stays.py
+++ src/features/flattened_features/loaders/load_icu_stays.py
@@ -3,18 +3,16 @@ from typing import Optional
 
 import pandas as pd
 from timeseriesflattener.utils import data_loaders
-from utils import (
-    load_dataset_from_file, 
-    DATA_PATH
-)
+from utils import DATA_PATH, load_dataset_from_file
 
 
 @data_loaders.register("icu_stays")
 def load_icu_stays(
     nrows: Optional[int] = None,
 ) -> pd.DataFrame:
-    """Load icu stays table. Drops admissions without chartevents data and duplicate rows. 
-    Returns a df with columns for patient_id, timestamp, and value.
+    """Load icu stays table. Drops admissions without chartevents data and
+    duplicate rows. Returns a df with columns for patient_id, timestamp, and
+    value.
 
     Args:
         nrows (int): Number of rows to load.
@@ -22,7 +20,9 @@ def load_icu_stays(
     Returns:
         pd.DataFrame: Icu stays table.
     """
-    icu_stays_file_path = DATA_PATH / "mimic-iii-clinical-database-1.4" / "ICUSTAYS.csv.gz"
+    icu_stays_file_path = (
+        DATA_PATH / "mimic-iii-clinical-database-1.4" / "ICUSTAYS.csv.gz"
+    )
 
     icu_stays = load_dataset_from_file(
         file_path=icu_stays_file_path,
@@ -38,7 +38,9 @@ def load_icu_stays(
     icu_stays["OUTTIME"] = pd.to_datetime(icu_stays["OUTTIME"])
 
     # Rename columns
-    icu_stays = icu_stays.rename(columns={"SUBJECT_ID": "patient_id", "OUTTIME": "timestamp", "LOS": "value"})
+    icu_stays = icu_stays.rename(
+        columns={"SUBJECT_ID": "patient_id", "OUTTIME": "timestamp", "LOS": "value"}
+    )
 
     return icu_stays.reset_index(drop=True)
 
